9308614996f62962a432ae601d853ce1482cdf3c
Grace <huang.yahui.grace@gmail.com>
2022-10-10 21:25:22 +0800

    新增localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mylocalstorage';
+  data: string = '';
+
+  myLoad() {
+    var myContent = localStorage.getItem("myContent");
+    this.data = myContent!;
+  }
+
+  mySave() {
+    var myContent = this.data;
+    localStorage.setItem("myContent", myContent);
+  }
 }
